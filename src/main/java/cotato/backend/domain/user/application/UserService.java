433e14a93a34c8456deb039eb5b6eb0765372ad2
he-on <hyeonjub2911@g.hongik.ac.kr>
2025-04-30 20:53:12 +0900

    feat : 사용자 서비스 로직 구현

--- src/main/java/cotato/backend/domain/user/application/UserService.java
+++ src/main/java/cotato/backend/domain/user/application/UserService.java
@@ -1,4 +1,37 @@
 package cotato.backend.domain.user.application;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cotato.backend.common.exception.EntityNotFoundException;
+import cotato.backend.common.exception.ErrorCode;
+import cotato.backend.domain.user.dao.UserRepository;
+import cotato.backend.domain.user.dto.response.UserResponse;
+import cotato.backend.domain.user.entity.UserEntity;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long save(String username, String email, String password) {
+        UserEntity user = UserEntity.builder()
+                .username(username)
+                .email(email)
+                .password(password)
+                .build();
+        return userRepository.save(user).getId();
+    }
+
+    public UserResponse findById(Long id) {
+        return UserResponse.from(
+                userRepository.findById(id)
+                        .orElseThrow(() -> new EntityNotFoundException(ErrorCode.NOT_FOUND))
+        );
+    }
 }
