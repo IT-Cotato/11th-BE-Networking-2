448400b4b6a04dc67cbd522fcd0e87ee0f9177f9
he-on <hyeonjub2911@g.hongik.ac.kr>
2025-04-30 20:42:34 +0900

    feat : 도메인 엔티티 및 enum 타입 정의

--- src/main/java/cotato/backend/domain/user/entity/UserEntity.java
+++ src/main/java/cotato/backend/domain/user/entity/UserEntity.java
@@ -1,4 +1,34 @@
 package cotato.backend.domain.user.entity;
 
-public class UserEntity {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+    @Entity
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Table(name = "user")
+    public class UserEntity {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(nullable = false, unique = true, length = 50)
+        private String username;
+
+        @Column(nullable = false, unique = true, length = 100)
+        private String email;
+
+        @Column(nullable = false)
+        private String password;
+
+        @Builder
+        public UserEntity(String username, String email, String password) {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
 }
