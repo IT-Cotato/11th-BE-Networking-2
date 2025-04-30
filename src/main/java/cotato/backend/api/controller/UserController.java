40c627c1997f427f1d33ed649a190ec64190298e
he-on <hyeonjub2911@g.hongik.ac.kr>
2025-04-30 20:33:10 +0900

    feat : 컨트롤러 계층 구현

--- src/main/java/cotato/backend/api/controller/UserController.java
+++ src/main/java/cotato/backend/api/controller/UserController.java
@@ -1,4 +1,38 @@
 package cotato.backend.api.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import cotato.backend.api.dto.response.DefaultIdResponse;
+import cotato.backend.common.dto.DataResponse;
+import cotato.backend.domain.user.application.UserService;
+import cotato.backend.domain.user.dto.request.UserRequest;
+import cotato.backend.domain.user.dto.response.UserResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<DataResponse<DefaultIdResponse>> save(@RequestBody UserRequest request) {
+        return ResponseEntity.ok(
+                DataResponse.created(
+                        DefaultIdResponse.of(
+                                userService.save(request.getUsername(), request.getEmail(), request.getPassword())
+                        )
+                )
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DataResponse<UserResponse>> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                DataResponse.from(userService.findById(id))
+        );
+    }
 }
