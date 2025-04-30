35899fea0dbed851fa028f84eeec9f75dbe3234d
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-04-30 18:45:57 +0900

    :sparkles: feat: 지원자 엔티티에 필드 추가 및 BaseEntity 상속
    - Constants : 도메인 관련 상수관리 Enum 클래스
    - Part : 지원자 파트 구분 Enum 클래스

--- src/main/java/cotato/backend/domain/applicant/entity/Applicant.java
+++ src/main/java/cotato/backend/domain/applicant/entity/Applicant.java
@@ -1,7 +1,13 @@
 package cotato.backend.domain.applicant.entity;
 
+import static cotato.backend.domain.applicant.enums.Constants.*;
+
+import cotato.backend.common.entity.BaseEntity;
+import cotato.backend.domain.applicant.enums.Part;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -15,18 +21,52 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "applicant")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Applicant {
+public class Applicant extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "example_id")
+	@Column(name = "applicant_id")
 	private Long id;
 
 	@Column(name = "name", nullable = false)
 	private String name;
 
+	@Column(name = "generation", nullable = false)
+	private int generation;
+
+	@Column(name = "age", nullable = false)
+	private int age;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "part", nullable = false)
+	private Part part;
+
+	@Column(name = "activity", nullable = false)
+	private int activity;
+
+	@Column(name = "growth", nullable = false)
+	private int growth;
+
+	@Column(name = "phone_number", nullable = false)
+	private String phoneNumber;
+
+	@Column(name = "likes", nullable = false)
+	private int likes;
+
 	@Builder
-	public Applicant(String name) {
+	public Applicant(String name, int generation, int age, Part part, int activity,
+		int growth, String phoneNumber) {
 		this.name = name;
+		this.generation = generation;
+		this.age = age;
+		this.part = part;
+		this.activity = activity;
+		this.growth = growth;
+		this.phoneNumber = phoneNumber;
+		this.likes = DEFAULT_LIKES.getValue();
+	}
+
+	public void addLike() {
+		this.likes++;
 	}
 }
