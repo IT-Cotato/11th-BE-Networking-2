caeb1e30496298568b37789024a8760f9491aad7
YoungjaeRo <ryj0330@naver.com>
2025-04-30 10:10:02 +0900

    chore : 사용하지 않는 import 삭제

--- src/main/java/cotato/backend/api/controller/ApplicantController.java
+++ src/main/java/cotato/backend/api/controller/ApplicantController.java
@@ -1,6 +1,5 @@
 package cotato.backend.api.controller;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
