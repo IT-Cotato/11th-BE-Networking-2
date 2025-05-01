9823ac6d4d82b418904e9a0dff0ff10db98c6252
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-05-01 15:02:41 +0900

    :recycle: refactor: @UtilityClass 적용 코드 리팩토링

--- src/main/java/cotato/backend/domain/utils/DataTimeUtils.java
+++ src/main/java/cotato/backend/domain/utils/DataTimeUtils.java
@@ -11,9 +11,9 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class DataTimeUtils {
 
-	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-	public static String format(LocalDateTime dateTime) {
+	public String format(LocalDateTime dateTime) {
 		return Optional.ofNullable(dateTime)
 			.map(dt -> dt.format(formatter))
 			.orElseThrow(() -> new AppException(ErrorCode.DATE_TIME_FORMAT_ERROR));
